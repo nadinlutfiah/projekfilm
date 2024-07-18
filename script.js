6ab8cc1b7fec65028adeb366362cd468fe8fa546
nadinlutfiah <nadinelutfiah17@gmail.com>
2024-07-18 10:01:40 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".watch-trailer").on("click", function() {
+        // Add code to play trailer video
+        alert("Trailer video will play here");
+    });
+
+    $(".watch-now").on("click", function() {
+        // Add code to play movie
+        alert("Movie will play here");
+    });
+});
